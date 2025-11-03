919ce1a8753ed24d774d03906cdb9601a2b47e29
HuongGiang <hgiang190325@gmail.com>
2025-11-03 13:14:40 +0700

    Cau_truc_du_an

--- ChatP2P/obj/Debug/net8.0-windows/ChatP2P.AssemblyInfo.cs
+++ ChatP2P/obj/Debug/net8.0-windows/ChatP2P.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatP2P")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04ea29763440ec4e41e879162a4e90f3995e3677")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatP2P")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatP2P")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
