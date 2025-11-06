1b9e5e46a7aa22a538411ddb33d023fa7ab32b04
TvhBao-23 <tranvohoaibao254@gmail.com>
2025-11-06 23:23:32 +0700

    Update ConversationModel and ConversationManager

--- ChatP2P/Model/ConversationModel.cs
+++ ChatP2P/Model/ConversationModel.cs
@@ -1,35 +1,200 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
 
 namespace ChatP2P.Model
 {
-    public class ConversationModel
+    /// <summary>
+    /// Lớp ConversationModel định nghĩa một cuộc hội thoại giữa người dùng hiện tại và một người dùng khác.
+    /// Chứa ObservableCollection tin nhắn và các thông tin liên quan đến cuộc trò chuyện.
+    /// </summary>
+    public class ConversationModel : INotifyPropertyChanged
     {
-        public string ConversationId { get; }
-        public UserModel Self { get; }
-        public UserModel Peer { get; }
-        public bool IsActive { get; set; }
-        public ObservableCollection<DataModel> Messages { get; }
+        private string endpoint;
+        public string Endpoint { get { return endpoint; } set { endpoint = value; } }
 
-        public ConversationModel(UserModel self, UserModel peer, bool isActive = true)
-        {
-            ConversationId = GenerateConversationId(self, peer);
-            Self = self;
-            Peer = peer;
-            IsActive = isActive;
-            Messages = new ObservableCollection<DataModel>();
+        private string name;
+        public string Name { get; set; }
+
+        private DateTime lastActivity;
+        public DateTime LastActivity 
+        { 
+            get => lastActivity; 
+            set 
+            { 
+                lastActivity = value; 
+                OnPropertyChanged(nameof(LastActivity));
+                OnPropertyChanged(nameof(LastActivityToString));
+            } 
+        }
+        public string LastActivityToString { get { return DateToString(lastActivity); } }
+
+        private bool unreadMessages = false;
+        public bool UnreadMessages 
+        { 
+            get { return unreadMessages; } 
+            set 
+            { 
+                unreadMessages = value; 
+                OnPropertyChanged(nameof(UnreadMessages));
+            } 
         }
 
-        public void Append(DataModel data)
-        {
-            Messages.Add(data);
+        private bool unreadBuzz = false;
+        public bool UnreadBuzz 
+        { 
+            get { return unreadBuzz; } 
+            set 
+            { 
+                unreadBuzz = value; 
+                OnPropertyChanged(nameof(UnreadBuzz));
+            } 
         }
 
-        private static string GenerateConversationId(UserModel a, UserModel b)
+        private bool isActive = false;
+        /// <summary>
+        /// Xác định conversation có đang active (có kết nối TCP) hay không
+        /// </summary>
+        public bool IsActive 
+        { 
+            get { return isActive; } 
+            set 
+            { 
+                isActive = value; 
+                OnPropertyChanged(nameof(IsActive));
+            } 
+        }
+
+        public string Username { get { return user?.Name ?? ""; } }
+        private UserModel user;
+        public UserModel User 
+        { 
+            get { return user; } 
+            set 
+            { 
+                user = value; 
+                OnPropertyChanged(nameof(Username));
+            } 
+        }
+
+        /// <summary>
+        /// ObservableCollection chứa tất cả các tin nhắn trong cuộc hội thoại
+        /// </summary>
+        private ObservableCollection<DataModel> messages = new ObservableCollection<DataModel>();
+        public ObservableCollection<DataModel> Messages { get { return messages; } }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
         {
-            var p1 = $"{a.Address}:{a.Port}";
-            var p2 = $"{b.Address}:{b.Port}";
-            return string.CompareOrdinal(p1, p2) <= 0 ? $"{p1}|{p2}" : $"{p2}|{p1}";
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ConversationModel() 
+        {
+            lastActivity = DateTime.Now;
+        }
+
+        public ConversationModel(string endpoint)
+        {
+            this.endpoint = endpoint;
+            lastActivity = DateTime.Now;
+        }
+
+        public ConversationModel(UserModel user)
+        {
+            this.user = user;
+            this.endpoint = user.Address;
+            this.name = user.Name;
+            lastActivity = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Nhận và thêm tin nhắn mới vào conversation
+        /// </summary>
+        public void ReceiveMessage(DataModel message)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                lastActivity = message.Date;
+                messages.Add(message);
+                if (User != null && !(ConversationManager.Instance.CurrentConversation == User.Address))
+                {
+                    unreadMessages = true;
+                    UnreadBuzz = false;
+                }
+                else
+                {
+                    unreadMessages = false;
+                }
+                OnPropertyChanged(nameof(LastActivity));
+                OnPropertyChanged(nameof(LastActivityToString));
+            });
+        }
+
+        /// <summary>
+        /// Nhận và xử lý tín hiệu buzz
+        /// </summary>
+        public void ReceiveBuzz(DataModel buzz)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                lastActivity = buzz.Date;
+                if (User != null && !(ConversationManager.Instance.CurrentConversation == User.Address))
+                {
+                    unreadBuzz = true;
+                    unreadMessages = false;
+                }
+                else
+                {
+                    unreadBuzz = false;
+                }
+                OnPropertyChanged(nameof(LastActivity));
+                OnPropertyChanged(nameof(LastActivityToString));
+            });
+        }
+
+        /// <summary>
+        /// Đánh dấu đã đọc tất cả tin nhắn
+        /// </summary>
+        public void MarkAsRead()
+        {
+            UnreadMessages = false;
+            UnreadBuzz = false;
+        }
+        private static string DateToString(DateTime dateTime)
+        {
+            int seconds = (int)(DateTime.Now - dateTime).TotalSeconds;
+
+            if (seconds < 30)
+            {
+                return "Just now";
+            }
+            else if (seconds < 60)
+            {
+                return $"{seconds} sec ago";
+            }
+            else if (seconds < 60 * 60)
+            {
+                int minutes = seconds / 60;
+                return $"{minutes} min ago";
+            }
+            else if (seconds < (60 * 60 * 24))
+            {
+                int hours = seconds / 60 / 60;
+                string tmp = hours == 1 ? "hour" : "hours";
+                return $"{hours} {tmp} ago";
+            }
+            else if (seconds < (60 * 60 * 24 * 30))
+            {
+                int days = seconds / 60 / 60 / 24;
+                string tmp = days == 1 ? "day" : "days";
+                return $"{days} {tmp} ago";
+            }
+            else
+            {
+                return "Long ago";
+            }
         }
     }
 }
