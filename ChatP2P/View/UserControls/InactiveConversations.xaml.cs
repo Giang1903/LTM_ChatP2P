509dd812129598783dc62424009a6256d1307411
Giang-boop <truonggiang02032005@gmail.com>
2025-11-10 16:02:05 +0700

    cập nhật

--- ChatP2P/View/UserControls/InactiveConversations.xaml.cs
+++ ChatP2P/View/UserControls/InactiveConversations.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ChatP2P.ViewModel;
+using ChatP2P.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,18 +12,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ChatP2P.View.UserControls
 {
-    /// <summary>
-    /// Interaction logic for InactiveConversations.xaml
-    /// </summary>
-    public partial class InactiveConversations : Window
+  
+    public partial class InactiveConversations : UserControl
     {
         public InactiveConversations()
         {
             InitializeComponent();
+            this.DataContext = new InactiveConversationsViewModel();
         }
     }
 }
