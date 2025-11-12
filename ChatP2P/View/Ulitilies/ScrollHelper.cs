828bd444d962b170586ef41aea05292e3836e010
nanhnanhh <anhngocle4128@gmail.com>
2025-11-12 22:41:49 +0700

    Cập nhật Conversation UI và ViewModel

--- ChatP2P/View/Ulitilies/ScrollHelper.cs
+++ ChatP2P/View/Ulitilies/ScrollHelper.cs
@@ -3,10 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
 
 namespace ChatP2P.View.Ulitilies
 {
     internal class ScrollHelper
     {
+        public static readonly DependencyProperty AutoScrollToEndProperty = DependencyProperty.RegisterAttached(
+            "AutoScrollToEnd", typeof(bool), typeof(ScrollHelper), new PropertyMetadata(false, OnAutoScrollToEndChanged));
+
+        public static bool GetAutoScrollToEnd(ScrollViewer scrollViewer)
+        {
+            return (bool)scrollViewer.GetValue(AutoScrollToEndProperty);
+        }
+
+        public static void SetAutoScrollToEnd(ScrollViewer scrollViewer, bool value)
+        {
+            scrollViewer.SetValue(AutoScrollToEndProperty, value);
+        }
+
+        private static void OnAutoScrollToEndChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ScrollViewer scrollViewer)
+            {
+                bool autoScrollToEnd = (bool)e.NewValue;
+                if (autoScrollToEnd)
+                {
+                    scrollViewer.ScrollChanged += ScrollViewer_ScrollChanged;
+                }
+                else
+                {
+                    scrollViewer.ScrollChanged -= ScrollViewer_ScrollChanged;
+                }
+            }
+        }
+
+        private static void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            var scrollViewer = (ScrollViewer)sender;
+            if (e.ExtentHeightChange > 0)
+            {
+                scrollViewer.ScrollToEnd();
+            }
+        }
     }
 }
